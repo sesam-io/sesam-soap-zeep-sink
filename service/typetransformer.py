416886c3e37d4f8abad02c4e99f7daed811c6c9a
Ashkan Vahidishams <ashkan.vahidishams@bouvet.no>
2017-09-01 10:15:21 +0200

    added back support for transit decoding of datetimes

--- service/typetransformer.py
+++ service/typetransformer.py
@@ -7,11 +7,18 @@ def transit_decode(part):
         for key, val in entity.items():
             if isinstance(val, dict) or isinstance(val,list):
                 transit_decode(val)
+            elif isinstance(val, str) and val.startswith("~t"):
+                entity[key] = transit_decode_datetime(val)
             elif isinstance(val, str) and val.startswith("$"):
-                entity[key]=transit_decode_datetime(val)
+                entity[key]=decode_date(val)
     return entity
 
+
 def transit_decode_datetime(val):
+    return datetime.datetime.strptime(val, '~t%Y-%m-%dT%H:%M:%S.%fZ')
+
+
+def decode_date(val):
     if len(val) is 11:
         return datetime.datetime.strptime(val, '$%Y-%m-%d').date()
     else:
